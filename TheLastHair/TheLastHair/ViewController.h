891e05d089dada1bd91e76b8a72f6e88ddf9464d
UDONKONET <udonkonet@UDONKONETnoMacBook.local>
2018-01-02 22:55:44 +0900

    空のプロジェクトを作成

---
+++ TheLastHair/TheLastHair/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheLastHair
+//
+//  Created by UDONKONET on 2018/01/02.
+//  Copyright © 2018年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheLastHair/TheLastHair/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheLastHair
+//
+//  Created by UDONKONET on 2018/01/02.
+//  Copyright © 2018年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

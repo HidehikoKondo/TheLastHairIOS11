37f4a04fa47ad6dd0ebe91f92e0152f647efec84
UDONKONET <udonkonet@UDONKONETnoMacBook.local>
2018-01-03 01:19:42 +0900

    ゲーム画面準備

---
+++ TheLastHair/TheLastHair/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  TheLastHair
+//
+//  Created by UDONKONET on 2018/01/03.
+//  Copyright © 2018年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ TheLastHair/TheLastHair/GameViewController.m
@@ -0,0 +1,37 @@
+//
+//  GameViewController.m
+//  TheLastHair
+//
+//  Created by UDONKONET on 2018/01/03.
+//  Copyright © 2018年 UDONKONET. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
